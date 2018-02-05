0b1373a982686f83b1278460dc8550154223d47e
Alexis Koss <alexis30@uw.edu>
2018-02-05 01:18:45 -0800

    added knock knock joke

--- ikid/KnockViewController.swift
+++ ikid/KnockViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class KnockViewController: UIViewController {
 
+    @IBOutlet weak var knockLabel: UILabel!
+    @IBOutlet weak var knockButton: UIButton!
+    var knock: Int = 1
+    var knockPartOne = "Knock! Knock!"
+    var knockPartTwo = "Who's there?"
+    var knockPartThree = "Broken pencil."
+    var knockPartFour = "Broken pencil who?"
+    var knockPartFive = "Forget it—this joke is pointless."
+    
+    
+    func flipFromLeft() {
+        UIView.transition(with: self.view, duration: 0.40, options: UIViewAnimationOptions.transitionFlipFromLeft, animations: nil, completion: nil)
+    }
+    
+    func flipFromRight() {
+        UIView.transition(with: self.view, duration: 0.40, options: UIViewAnimationOptions.transitionFlipFromRight, animations: nil, completion: nil)
+    }
+    
+    @IBAction func knockAnswer(_ sender: Any) {
+        if(knock == 6) {
+            knock = 2
+        } else {
+            knock += 1
+        }
+        switch knock {
+        case 2:
+            knockLabel.text = knockPartTwo
+            knockButton.setTitle("Next", for: .normal)
+            flipFromRight()
+        case 3:
+            knockLabel.text = knockPartThree
+            flipFromLeft()
+        case 4:
+            knockLabel.text = knockPartFour
+            flipFromRight()
+        case 5:
+            knockLabel.text = knockPartFive
+            knockButton.setTitle("Back", for: .normal)
+            flipFromLeft()
+        default:
+            knockLabel.text = knockPartOne
+            knockButton.setTitle("Answer", for: .normal)
+            flipFromLeft()
+        }
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
